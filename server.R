aebcc09e0175fd2a6f6f6d55fe75284d50d5c736
Lena Jiricka <lenajiricka@hotmail.com>
2021-01-21 18:07:14 +0100

    added first draft of splines

--- server.R
+++ server.R
@@ -40,5 +40,53 @@ function(input, output, session){
     #############################################
     
     
+    
+    output$plot.bs <- renderPlotly({
+        var_list <- var_list_reac()
+        data <- var_list$data
+        
+        x <- data[,var_list$x]
+        
+        df <- input$df.bs
+        degree <- input$degree.bs
+        
+        b <- bs(x, degree=degree, knots=quantile(x, c(1/3, 2/3)))
+
+        #push up to data creation - no need for recalc every time
+        data <- data %>% 
+            group_by(!!sym(var_list$x)) %>% 
+            mutate(y_mean = mean(!!sym(var_list$y)))
+        
+        intercept <- input$intercept.bs
+        spline <- apply(b, 1, function(x, coefs = NULL) {
+            res <- 0
+            for(i in x){
+                res <- res + i
+            }
+            res
+        })
+
+        spline <- intercept + spline
+        p <- ggplot(data=data)
+        if(input$add_y.bs){
+            p <- p + geom_point(aes(x=!!sym(var_list$x), y=!!sym(var_list$y)), color = "lightgrey")
+        }
+        if(input$add_mean.bs){
+            p <- p + geom_line(aes(x=!!sym(var_list$x), y = y_mean), color = "blue")
+        }
+        if(input$add_knots_pos.bs){
+            knots <- attr(b, "knots")
+            for(i in knots){
+                p <- p + geom_vline(xintercept=i, color = "red")
+            }
+        }
+        
+        p <- p +geom_line(aes(x=!!sym(var_list$x), y = spline)) + 
+                theme_minimal() +
+                ylab(var_list$y) 
+        ggplotly(p)
+    })
+    
+    
     session$onSessionEnded(stopApp) #automatically stop when closing browser
 }
--- tabs/a_bsplines.R
+++ tabs/a_bsplines.R
@@ -1,6 +1,38 @@
 bsplines <- tabPanel("B-Splines", id="bsplines", class="fade", value = "bsplines",br(),
-            HTML('
-                            <h1> Hi you all, nice to see you again! </h1>
-                            <br> <p> You are on tab splines </p>
-            ')
-)
+            sidebarPanel(class="input_class", id = "inputs.fp",
+                fluidRow(column(7, offset=0, h4("Input parameters"))
+                ), 
+                fluidRow(
+                    column(6,
+                        sliderInput("degree.bs", "Degree", min=1, max=4, value=1, step=1)
+                    ),
+                    column(6,
+                        
+                    )
+                ), 
+                fluidRow(
+                    column(6,
+                        sliderInput("intercept.bs", "Intercept", min=1, max=100, value=0, step=0.1)
+                    ),
+                    column(6,
+                        
+                    )
+                ), 
+                fluidRow(
+                    column(3, offset=0, 
+                           materialSwitch(inputId = "add_y.bs", label = "Add response", status = "primary", right = FALSE)
+                    ), 
+                    column(3, offset=0, 
+                           materialSwitch(inputId = "add_mean.bs", label = "Add mean", status = "primary", right = FALSE)
+                    ),
+                    column(3, offset=0, 
+                           materialSwitch(inputId = "add_knots_pos.bs", label = "Add knots position", status = "primary", right = FALSE)
+                    )
+                ), 
+                fluidRow(
+                  column(12, offset=0, actionButton("reset_input.fp", "Reset inputs"))
+                )
+),mainPanel(
+    plotlyOutput("plot.bs")
+  )
+) 
