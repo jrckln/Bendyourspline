d7f05a1dab0576f916146ec361ba021f956a6088
Lena Jiricka <lenajiricka@hotmail.com>
2021-02-09 11:16:30 +0100

    added: natural splines tab

--- server.R
+++ server.R
@@ -392,5 +392,359 @@ function(input, output, session){
     observeEvent(input$reset_input.bs, {
         shinyjs::reset("inputs.bs") #id of tab to reset
     })
+    
+    #############################################
+    #######      Natural-splines    #############
+    #############################################
+    
+    #dynamic insert of slider for positions of knots
+    inserted.pos.nsp <- c()
+    #coefficient values - save for possibility to adjust with buttons
+    val.coefs.nsp <- c(0,0,0) # take care of default vals here
+    # does not need to be reactive since it should always be called with getcoef.nsp()
+    
+    range.coefs.nsp <- reactiveValues(range.coef.left = -1, range.coef.right = 1)
+    observeEvent(input$increase_range.nsp, {
+        range.coefs.nsp$range.coef.left <- range.coefs.nsp$range.coef.left-1
+        range.coefs.nsp$range.coef.right <- range.coefs.nsp$range.coef.right+1
+    })
+    observeEvent(input$decrease_range.nsp, {
+        range.coefs.nsp$range.coef.left <- range.coefs.nsp$range.coef.left+1
+        range.coefs.nsp$range.coef.right <- range.coefs.nsp$range.coef.right-1
+    })
+    #update range of coefficient sliders
+    observe({
+        num <- 1 + input$nknots.nsp
+        #get values of coefficients: 
+        ind <- paste0("coef", 1:num, "_inner")
+        for(i in ind){
+            updateSliderInput(session, i, max = range.coefs.nsp$range.coef.right, min=range.coefs.nsp$range.coef.left)
+        }
+    })
+    
+    
+    observeEvent(input$nknots.nsp, {
+        if(length(inserted.pos.nsp)!=0){ #case of update
+            if(length(inserted.pos.nsp)>as.numeric(input$nknots.nsp)){
+                #remove difference: 
+                num <- length(inserted.pos.nsp) - input$nknots.nsp
+                for(i in 1:num){
+                  removeUI(
+                    selector = paste0('#', inserted.pos.nsp[length(inserted.pos.nsp)]) #remove the last ones
+                  )
+                    inserted.pos.nsp <<- inserted.pos.nsp[-length(inserted.pos.nsp)]
+                }
+            }else if(length(inserted.pos.nsp)<as.numeric(input$nknots.nsp)){
+                default.pos.knots.nsp <- seq.int(from = 0, to = 1, length.out = input$nknots.nsp + 2)[-c(1, input$nknots + 2)]
+                var_list <- var_list_reac()
+                data <- var_list$data
+                x <- data[,var_list$x]
+                default.pos.knots.nsp <- quantile(x, default.pos.knots.nsp)
+                num <- (length(inserted.pos.nsp)+1):input$nknots.nsp
+                id <- paste0('nsp_pos', num)
+                for(i in 1:length(num)){
+                    insertUI(
+                        selector = '#placeholder_pos_nsp',
+                        ui = tags$div(sliderInput(paste0(id[i], "_inner"), label = paste0("Position of knot ", 
+                                                                                          length(inserted.pos.nsp)+i), 
+                                                  value=default.pos.knots.nsp[i], step=0.01, 
+                                                  min=min(x), max=max(x)), id=id[i])
+                    )
+                    inserted.pos.nsp <<- c(inserted.pos.nsp, id[i])
+                }
+            }
+        }else{ #case of init
+            default.pos.knots.nsp <- seq.int(from = 0, to = 1, length.out = input$nknots.nsp + 2)[-c(1, input$nknots + 2)]
+            var_list <- var_list_reac()
+            data <- var_list$data
+            x <- data[,var_list$x]
+            default.pos.knots.nsp <- quantile(x, default.pos.knots.nsp)
+            num <- 1:input$nknots.nsp
+            id <- paste0('nsp_pos', num)
+            for(i in 1:length(num)){
+                insertUI(
+                    selector = '#placeholder_pos_nsp',
+                    ui = tags$div(sliderInput(paste0(id[i], "_inner"), label = paste0("Position of knot ", i), 
+                                              value=default.pos.knots.nsp[i], step=0.01, 
+                                              min=min(x), max=max(x)), id=id[i])
+                )
+                inserted.pos.nsp <<- c(inserted.pos.nsp, id[i])
+            }
+        }
+    })
+    
+    #dynamic insert of slider for coefficients for each spline basis function
+    inserted.coef.nsp <- c()
+    observeEvent(c(input$nknots.nsp), {
+        num <- 1 + input$nknots.nsp #+1 for intercept but intercept is extra
+        if(length(inserted.coef.nsp)!=0){ #case of update
+            #num is the number of desired inputs
+            if(length(inserted.coef.nsp)>num){
+                if(length(inserted.coef.nsp)>0){
+                    toomuch <- length(inserted.coef.nsp)-num
+                    for(i in 1:toomuch){
+                      removeUI(
+                        selector = paste0('#', inserted.coef.nsp[length(inserted.coef.nsp)])
+                      )
+                    inserted.coef.nsp <<- inserted.coef.nsp[-length(inserted.coef.nsp)]
+                    }
+                }
+            }else if(length(inserted.coef.nsp)<num){
+                toinsert <- (length(inserted.coef.nsp)+1):num
+                id <- paste0('nsp_coef', toinsert)
+                for(i in 1:length(toinsert)){
+                    insertUI( #place inserted UI in div for complete removal
+                        selector = '#placeholder_coef_nsp',
+                        ui = tags$div(
+                            actionButton(paste0(id[i], "_inner_minus"), "", icon = icon("minus-square"), 
+                                         style='padding:4px; font-size:80%; vertical-align: -150%;background: #D6D6D6;', class="minus"),
+                            sliderInput(paste0(id[i], "_inner"), label = paste0("Coefficient ", 
+                                                                                          length(inserted.coef.nsp)+i), 
+                                                  value=1, step=0.01, min=-1, max=1, width='80%'), 
+                            actionButton(paste0(id[i], "_inner_plus"), "", icon = icon("plus-square"), 
+                                         style='padding:4px; font-size:80%; vertical-align: -150%; background: #D6D6D6;', class="plus"),
+                            id=id[i]
+                        )
+                    )
+                    inserted.coef.nsp <<- c(inserted.coef.nsp, id[i])
+                }
+            }
+        } else { #case of init
+            id <- paste0('nsp_coef', 1:num)
+            for(i in 1:num){
+                insertUI(
+                    selector = '#placeholder_coef_nsp',
+                    ui = tags$div(
+                        actionButton(paste0(id[i], "_inner_minus"), "", icon = icon("minus-square"), 
+                                     style='padding:4px; font-size:80%; vertical-align: -150%; background: #D6D6D6;', class="minus"), 
+                        # class to control only this button group
+                        # and get last id with JS (see tab bsplines)
+                        sliderInput(paste0(id[i], "_inner"), label = paste0("Coefficient ", i), 
+                                              value=1, step=0.01, 
+                                              min=-1, max=1, width='80%'),
+                        actionButton(paste0(id[i], "_inner_plus"), "", icon = icon("plus-square"), 
+                                         style='padding:4px; font-size:80%; vertical-align: -150%; background: #D6D6D6;', class="plus"),
+                        id=id[i])
+                )
+                inserted.coef.nsp <<- c(inserted.coef.nsp, id[i])
+            }
+        }
+        
+    })
+    
+    getpos.nsp <- reactive({
+        #get values of knot positions: 
+        ind <- match(paste0("nsp_pos", 1:input$nknots.nsp, "_inner"), names(input))
+        ind <- ind[!(is.na(ind))]
+        pos <- c()
+        for(i in ind){
+            pos <- c(pos, input[[names(input)[i]]])
+        }
+        pos
+    })
+    
+    getcoef.nsp <- reactive({
+        num <- 1 + input$nknots.nsp
+        #get values of coefficients: 
+        ind <- match(paste0("nsp_coef", 1:num, "_inner"), names(input))
+        ind <- ind[!(is.na(ind))]
+        coef <- c()
+        for(i in ind){
+            coef <- c(coef, input[[names(input)[i]]])
+        }
+        coef
+    })
+    
+    getintercept.nsp <- reactive({
+        if(input$adjust_intercept.nsp){
+            var_list <- var_list_reac()
+            data <- var_list$data
+            x <- data[,var_list$x]
+            pos <- getpos.nsp()
+            b <- ns(x,knots=pos)
+            coefs <- getcoef.nsp()
+            spline <- apply(b, 1, function(x, coefs.in = coefs) {
+                res <- 0
+                for(i in 1:length(x)){
+                    res <- res + coefs.in[i]*x[i]
+                }
+                res
+            })
+            intercept <- opt.intercept(fitted=spline, data=data[,var_list$y], interval=c(min(data[,var_list$y]), max(data[,var_list$y])))$minimum
+            return(intercept)
+        } else {
+            return(input$intercept.nsp)
+        }
+    })
+    
+    output$intercept.nsp <- renderUI({
+        intercept.val <- getintercept.nsp()
+        if(input$adjust_intercept.nsp){
+            div(HTML(paste0("Intercept fitted using LS to: ", round(intercept.val, 3))))
+        }
+        else {
+             div(HTML(paste0("Intercept set to: ", round(intercept.val, 3))))
+        }
+    })
+    
+    getid_minus.nsp <- reactive({
+        #returns quoted expression
+        num <- 1 + input$nknots.nsp
+        ind <- match(paste0("nsp_coef", 1:num, "_inner_minus"), names(input))
+        ind <- ind[!(is.na(ind))]
+        ind
+    })
+    
+    getid_plus.nsp <- reactive({
+        #returns quoted expression
+        num <- 1 + input$nknots.nsp
+        ind <- match(paste0("nsp_coef", 1:num, "_inner_plus"), names(input))
+        ind <- ind[!(is.na(ind))]
+        ind
+    })
+    
+    #trigger when any minus button was clicked: modifies input value
+    observeEvent(input$last_btn_minus, {
+        coefs <- getcoef.nsp()
+        val.coefs.nsp <- coefs
+        #determine button of which coef id was clicked: id of button is coef<idnumber>_inner_minus
+        id.coef <- gsub("_minus", "", input$last_btn_minus)
+        ind <- as.numeric(gsub("_inner", "", gsub("coef","",id.coef)))
+        val <- getcoef.nsp()
+        val <- val[ind] -0.01
+        updateSliderInput(session, inputId = id.coef, value = val)
+    })
+    
+    observeEvent(input$last_btn_plus, {
+        coefs <- getcoef.nsp()
+        val.coefs.nsp <- coefs
+        #determine button of which coef id was clicked: id of button is coef<idnumber>_inner_minus
+        id.coef <- gsub("_plus", "", input$last_btn_plus)
+        ind <- as.numeric(gsub("_inner", "", gsub("coef","",id.coef)))
+        val <- getcoef.nsp()
+        val <- val[ind] + 0.01
+        updateSliderInput(session, inputId = id.coef, value = val)
+    })
+    
+    output$plot.nsp <- renderPlotly({
+        var_list <- var_list_reac()
+        data <- var_list$data
+        
+        x <- data[,var_list$x]
+        
+        pos <- getpos.nsp()
+        b <- ns(x, knots=pos)
+
+        #TODO: push up to data creation - no need for recalc every time
+        data <- data %>% 
+            group_by(!!sym(var_list$x)) %>% 
+            mutate(y_mean = mean(!!sym(var_list$y)))
+        
+        coefs <- getcoef.nsp()
+        spline <- apply(b, 1, function(x, coefs.in = coefs) {
+            res <- 0
+            for(i in 1:length(x)){
+                res <- res + coefs.in[i]*x[i]
+            }
+            res
+        })
+        
+        intercept <- getintercept.nsp()
+        spline <- intercept + spline
+        p <- ggplot(data=data)
+        if(input$add_y.nsp){
+            p <- p + geom_point(aes(x=!!sym(var_list$x), y=!!sym(var_list$y)), color = "lightgrey")
+        }
+        if(input$add_mean.nsp){
+            p <- p + geom_line(aes(x=!!sym(var_list$x), y = y_mean), color = "blue")
+        }
+        if(input$add_knots_pos.nsp){
+            knots <- attr(b, "knots")
+            for(i in knots){
+                p <- p + geom_vline(xintercept=i, color = "red", linetype="dashed")
+            }
+        }
+        
+        p <- p +geom_line(aes(x=!!sym(var_list$x), y = spline)) + 
+                theme_minimal() +
+                ylab(var_list$y) 
+        ggplotly(p)
+    })
+    
+    output$basis_plot.nsp<- renderPlotly({
+        var_list <- var_list_reac()
+        data <- var_list$data
+        x <- data[,var_list$x]
+        pos <- getpos.nsp()
+        b <- ns(x, knots=pos)
+        all.knots <- sort(c(attr(b,"Boundary.knots") ,attr(b, "knots")))
+        bounds <- range(all.knots)
+        knot.values <- set_colnames(predict(b, all.knots),str_c("S", seq_len(ncol(predict(b, all.knots)))))
+        newx <- seq(bounds[1], bounds[2], length.out = 100+1)
+        interp.values <- set_colnames(predict(b, newx),str_c("S", seq_len(ncol(predict(b, newx)))))
+        knot.df <- melt(data.frame(x=all.knots, knot.values), id.vars="x", variable.name="Spline", value.name="y")
+        interp.df <- melt(data.frame(x=newx, interp.values),id.vars="x", variable.name="Spline", value.name="y")
+        p <- ggplot(interp.df) +
+            aes(x=x, y=y, color=Spline) +
+            geom_line() +
+            scale_color_manual(values = col) + theme_minimal() + theme(legend.position = "none") + 
+            ggtitle("Spline basis functions")
+        ggplotly(p)
+    })
+    
+    calcR2.nsp <- reactive({
+        var_list <- var_list_reac()
+        data <- var_list$data
+        x <- data[,var_list$x]
+        pos <- getpos.nsp()
+        b <- ns(x, knots=pos)
+        coefs <- getcoef.nsp()
+        spline <- apply(b, 1, function(x, coefs.in = coefs) {
+            res <- 0
+            for(i in 1:length(x)){
+                res <- res + coefs.in[i]*x[i]
+            }
+            res
+        })
+        spline <- spline + getintercept.nsp()
+        model <- lm(as.formula(paste0(var_list$y, " ~ ns(", var_list$x, ", df=", 1+length(pos),")")), data=data)
+        fitted <- model$fitted
+        p <- model$rank
+        sstot <- sum((data[,var_list$y]-mean(data[,var_list$y]))^2) #total sum of squares
+        ssres <- sum((data[, var_list$y]-spline)^2)  #residual sum of squares
+        ssres_fitted <- sum((data[, var_list$y]-fitted)^2)  #residual sum of squares fitted
+        c(1-ssres/sstot, 1-ssres_fitted/sstot, p)
+    })
+    
+    calcadjR2.nsp <- reactive({
+        res <- calcR2.nsp()
+        R2 <- res[1]
+        maxR2 <- res[2]
+        p <- res[3]
+        var_list <- var_list_reac()
+        data <- var_list$data
+        if(input$gender == "Both"){
+            sex_ind <- "both"
+        } else {
+            sex_ind <- gender[input$gender]
+        }
+        index <- paste0(input$sample.size,"_" ,sex_ind)
+        c(R2, 1-(1-R2)*(nrow(data)-1)/(nrow(data)-1-p), maxR2)
+    })
+    
+    output$stats.nsp <-  renderUI({
+        stats <- calcadjR2.nsp()
+        nknots <- input$nknots.nsp
+        HTML(paste0("R <sup>2</sup>: ", round(stats[1], 3), "<br> adj. R <sup>2</sup>: ", round(stats[2], 3), 
+                    "<br> max. R <sup>2</sup> for ", nknots," internal knots: ", round(stats[3], 3)))
+    })
+    
+    #reset button 
+    observeEvent(input$reset_input.nsp, {
+        shinyjs::reset("inputs.nsp") #id of tab to reset
+    })
+    
+    
+    
     session$onSessionEnded(stopApp) #automatically stop when closing browser
 }
