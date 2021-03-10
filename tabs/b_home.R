7a449950aac4be108f4353f65790112c9b41e03d
Lena Jiricka <lenajiricka@hotmail.com>
2021-03-10 14:52:48 +0100

    added home tab

--- tabs/b_home.R
+++ tabs/b_home.R
@@ -1,6 +1,3 @@
-home <- tabPanel("Home", value="Home", id="home",class="active", #class="my_style_1", #specify css style in tags$head in home
-            HTML('
-                <h1> Hi you all, nice to see you again! </h1>
-                <br> <p> You are on tab home </p>
-')
+home <- tabPanel("Home", value="Home", id="home",class="active", 
+            includeHTML("www/home.html")
 )
