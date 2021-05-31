2e0ef565c578251ebea2273620c28221556c8adb
Lena Jiricka <lenajiricka@hotmail.com>
2021-05-31 10:27:20 +0200

    refined test exercise

--- data/exercises.R
+++ data/exercises.R
@@ -2,19 +2,25 @@ exercises <- list(
     "test"= list(
         "instructions" = 
         c(
+        'Switch to Fractional polynomials in the methods section and use Bmi ~ Age as dataset. Make sure that response, optimal fit and loess smoother are turned off.',
         'Move the slider of coefficient 1 to 1 and see how the response curve reacts!', 
         'Set the second power to 2 and move the associated coefficient to 1!', 
         'Now move coefficient 1 to -1!', 
         'Now set coefficient 1 to +1 and coefficient 2 to -1!', 
-        'How can you adjust the curve to get the peak of the response function at approximately 40 years? 
-        (answer: move Coeff_1 to approx. 0.80, in general: Coeff_2 = - coeff_1/(40/100)/2, follows from setting 
-        first derivative to 0)', 
+        'How can you adjust the curve to get the peak of the response function at approximately 40 years?', 
         'Now change the second power to 3, then to 1, 0, -1 etc. See how the response curve reacts!', 
-        '8)	Set both powers to 2. The two powers are now set to equal numbers, which defines a ‘repeated power’, and in 
-        this case the second term is also multiplied with log(x). (Check the FP-formula!)', 
+        'Set both powers to 2. The two powers are now set to equal numbers, which defines a <b> repeated power </b>, and in 
+        this case the second term is also multiplied with log(x). (Check the formula)', 
         'With the powers at 2 and 2, try to adjust the coefficients such that you approximately get a U-shaped response function!',
-        ': keeping the powers as they are, try to ‘invert’ this U into a ∩ only with the coefficient sliders!: keeping the powers as they are,
-        try to ‘invert’ this U into a ∩ only with the coefficient sliders!'
+        'Keeping the powers as they are, try to ‘invert’ this U into a ∩ only with the coefficient sliders!'
         ), 
-        "validate" = c("TRUE", "input$power2.fp == 2", "TRUE", "TRUE", "TRUE", "TRUE", "TRUE", "TRUE" ))
+        "validate" = c("input$tabsetmethods == 'fp' & input$variable == 'Bmi ~ Age' & !input$add_y.fp & !input$add_loess_fp & !input$add_optfit_fp",
+            "input[['val_coef1_fp-coef']] == 1", 
+            "input$power2.fp == 2 & input[['val_coef2_fp-coef']] == 1", 
+            "input[['val_coef1_fp-coef']] == -1",
+            "input[['val_coef1_fp-coef']] == 1 & input[['val_coef2_fp-coef']] == -1", 
+            "input[['val_coef2_fp-coef']] >= -(input[['val_coef1_fp-coef']]/(40/100))/2 - 0.05 & input[['val_coef2_fp-coef']] <= -(input[['val_coef1_fp-coef']]/(40/100))/2 + 0.05", 
+            "TRUE", 
+            "input$power2.fp == 2 & input$power1.fp == 2", 
+            "TRUE" ))
 )
